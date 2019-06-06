e091b1f739ecf4fad1958519724afac193ad8628
Suraj Patil <surajp815@gmail.com>
2019-06-06 16:12:42 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -9,18 +9,23 @@ REQUIRED = ['requests']
 setuptools.setup(
     name="wynum",
     version="0.0.1",
-    author="the-groot",
-    author_email="suraj@wynum.com",
+    author="patil-suraj",
+    author_email="surajp815@gmail.com",
     description="Wynum API client",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # TODO add url
-    url="", 
+    url="https://github.com/wynum/wynum-python",
     packages=setuptools.find_packages(),
     install_requires=REQUIRED,
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License", # change later
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers"
     ],
+    project_urls={
+        'Documentation': 'https://github.com/wynum/wynum-python',
+        'Source': 'https://github.com/wynum/wynum-python',
+    },
 )
